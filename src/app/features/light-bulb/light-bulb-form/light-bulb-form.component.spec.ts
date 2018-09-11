dabb59e063fb7fbae5c4ab9134831b734cdb4998
machiningcloud\russell.frame <russell.frame@machiningcloud.com>
2018-09-10 23:53:07 -0700

    add unit tests for the light-bulb form component

--- src/app/features/light-bulb/light-bulb-form/light-bulb-form.component.spec.ts
+++ src/app/features/light-bulb/light-bulb-form/light-bulb-form.component.spec.ts
@@ -7,6 +7,7 @@ import { ActivatedRoute, Data, Router } from '@angular/router';
 import * as testMocks from '../../../models/helpers/test-mocks';
 import { of as observableOf } from 'rxjs';
 import { LightBulbResult } from '../../../models/classes/light-bulb-result';
+import { By } from '@angular/platform-browser';
 import { MatFormFieldModule, MatInputModule } from '@angular/material';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -62,7 +63,6 @@ describe('LightBulbFormComponent', () => {
     });
 
     it('should create the proper form', async(() => {
-
       component.formGroup$
         .toPromise()
         .then((fg) => {
@@ -70,5 +70,91 @@ describe('LightBulbFormComponent', () => {
           expect(fg.controls.people.value).toEqual(lightBulbResult.people);
         });
     }));
+
+    it('should update the values of the form to that of the form group', () => {
+      const lighbulbsInput = fixture.debugElement.query(By.css('#txtBoxLightBulbs'));
+      const peopleInput = fixture.debugElement.query(By.css('#txtBoxPeople'));
+
+      expect(+lighbulbsInput.nativeElement.value).toEqual(lightBulbResult.lightbulbs);
+      expect(+peopleInput.nativeElement.value).toEqual(lightBulbResult.people);
+    });
   });
+
+  describe('Empty Form Group', () => {
+    const lightBulbResult = null;
+    let formGroup = null;
+    beforeEach(() => {
+      mocks.route.data = observableOf({
+        lightBulbResult
+      } as Data);
+
+      spyOn(component, 'submit')
+        .and
+        .callFake((event, fGroup) => { formGroup = fGroup; });
+
+      fixture.detectChanges();
+    });
+
+    it('should mark the input invalid if a non digit is input', () => {
+      const lighbulbsInput = fixture.debugElement.query(By.css('#txtBoxLightBulbs'));
+      lighbulbsInput.nativeElement.value = 'Test';
+      lighbulbsInput.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const peopleInput = fixture.debugElement.query(By.css('#txtBoxPeople'));
+      peopleInput.nativeElement.value = 'Test';
+      peopleInput.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const form = fixture.debugElement.query(By.css('form'));
+      form.triggerEventHandler('submit', null);
+      fixture.detectChanges();
+
+      expect(component.submit).toHaveBeenCalledTimes(1);
+      expect(formGroup.controls.lightbulbs.valid).toBeFalsy();
+      expect(formGroup.controls.people.valid).toBeFalsy();
+    });
+
+    it('should mark the input invalid if a negative digit is input', () => {
+      const lighbulbsInput = fixture.debugElement.query(By.css('#txtBoxLightBulbs'));
+      lighbulbsInput.nativeElement.value = -32;
+      lighbulbsInput.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const peopleInput = fixture.debugElement.query(By.css('#txtBoxPeople'));
+      peopleInput.nativeElement.value = -32;
+      peopleInput.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const form = fixture.debugElement.query(By.css('form'));
+      form.triggerEventHandler('submit', null);
+      fixture.detectChanges();
+
+      expect(component.submit).toHaveBeenCalledTimes(1);
+      expect(formGroup.controls.lightbulbs.valid).toBeFalsy();
+      expect(formGroup.controls.people.valid).toBeFalsy();
+    });
+
+    it('should mark the input valid if a positive digit is input', () => {
+      const lighbulbsInput = fixture.debugElement.query(By.css('#txtBoxLightBulbs'));
+      lighbulbsInput.nativeElement.value = 32;
+      lighbulbsInput.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const peopleInput = fixture.debugElement.query(By.css('#txtBoxPeople'));
+      peopleInput.nativeElement.value = 32;
+      peopleInput.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const form = fixture.debugElement.query(By.css('form'));
+      form.triggerEventHandler('submit', null);
+      fixture.detectChanges();
+
+      expect(component.submit).toHaveBeenCalledTimes(1);
+      expect(formGroup.controls.lightbulbs.valid).toBeTruthy();
+      expect(formGroup.controls.people.valid).toBeTruthy();
+    });
+
+  });
+
 });
